f5a831425afffe8f55905b0aaa990b52b7946591
jawolf17 <jawolf@buffalo.edu>
2016-09-29 15:58:44 -0400

    Minor Bug Fix

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
 def login():
     return "LOGIN"
 
-@app.route('/create-user',methods=["GET","POST"])
+@app.route('/create-user',methods=["POST"])
 def create_user():
     #Get Request
     data = json.loads(request.get_json(force = True))
@@ -23,11 +23,16 @@ def create_user():
     u_id = str(uuid.uuid4())
 
     #format data for insertions
-    user = ((data["username"],data["password"],"","","","","","","","",u_id)
+    user = ((data["username"],data["password"],"","","","","","","","",u_id),)
 
     with connection:
         cur = connection.cursor()
-        cur.executeMany("INSERT INTO Cars VALUES (?,?,?,?,?,?,?,?,?,?)", user)
+        cur.executeMany("INSERT INTO namePass VALUES (?,?,?,?,?,?,?,?,?,?)", user)
         connection.commit()
 
-    return "Created"
+    #Create Response
+    response = {"message": "A new user has successfully been added to the database. ",
+                "code": 200}
+
+    print "[POST] NEW USER CREATION: New user has been added to the database"
+    return json.jsonify(response)
