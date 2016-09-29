6768e1d9d448aad11b4e091a9538fb399beb4364
jawolf17 <jawolf@buffalo.edu>
2016-09-29 14:28:00 -0400

    Created Handler for Creating a new User

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import json, sqlite3, uuid
 
 app=Flask(__name__)
 
 @app.route('/')
 def main():
     return "It's a server!"
+
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    return "LOGIN"
+
+@app.route('/create-user',methods=["GET","POST"])
+def create_user():
+    #Get Request
+    data = json.loads(request.get_json(force = True))
+    #connect to db
+    connection = sqlite3.connect('db/accounts.db')
+
+    #format data for insertions
+    user = ((data["username"],data["password"],"","","","","","","",""))
+
+    with connection:
+        cur = connection.cursor()
+        cur.executeMany("INSERT INTO Cars VALUES (?,?,?,?,?,?,?,?,?,?)", user)
+        connection.commit()
+
+    return "Created"
